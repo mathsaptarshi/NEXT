6a7671e8fd0d4558fb5f69e7b109e4b6c801a2e6
Saptarshi Biswas <mathsaptarshi@gmail.com>
2019-03-05 00:02:54 +0530

    Initial

---
+++ .next/static/webpack/static/development/pages/post.js.b752f587ac8b1263c5c0.hot-update.js
@@ -0,0 +1,30 @@
+webpackHotUpdate("static\\development\\pages\\post.js",{
+
+/***/ "./pages/post.js":
+/*!***********************!*\
+  !*** ./pages/post.js ***!
+  \***********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_MyLayout_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/MyLayout.js */ "./components/MyLayout.js");
+/* harmony import */ var isomorphic_unfetch__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! isomorphic-unfetch */ "./node_modules/isomorphic-unfetch/browser.js");
+/* harmony import */ var isomorphic_unfetch__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(isomorphic_unfetch__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+
+var Post = function Post(props) {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_MyLayout_js__WEBPACK_IMPORTED_MODULE_1__["default"], null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, props.show.name), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, props.show.summary.replace(/<[/]?p>/g, '')), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    src: props.show.image.medium
+  }));
+};
+
+/***/ })
+
+})
+//# sourceMappingURL=post.js.b752f587ac8b1263c5c0.hot-update.js.map
